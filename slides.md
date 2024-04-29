23448afc6d4a770b12c877b698ed87562de81bfd
mnenie <121057011+mneniee@users.noreply.github.com>
2024-04-30 00:04:22 +0300

    🎉 init repo

---
+++ slides.md
@@ -0,0 +1,21 @@
+---
+theme: vuetiful
+clicks: 1
+altCover: false
+mdc: true
+growSeed: 4
+highlighter: shiki
+---
+
+# Мощь двустороннего связывания во Vue.js
+
+The RTU MIREA Conference (May 2024)
+
+---
+
+# Обо мне
+
+<AboutMe />
+
+---
+
---
+++ theme/layouts/intro.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  author: string,
+  name: string,
+  telegram?: string,
+  github?: string,
+}>()
+</script>
+<template>
+  <div class="slidev-layout flex flex-col justify-center items-center">
+    <span text-5xl>{{ name }}</span>
+    <span text-3xl pt6>{{ author }}</span>
+
+    <div  absolute pos="bottom-xl right-xl" flex="~ col">
+      <div v-if="telegram">
+        {{ telegram }}
+      </div>
+      <div v-if="github">
+        {{ github }}
+      </div>
+    </div>
+  </div>
+</template>
